1c6516511fc1b792b5016627d4ff3dde045ae70e
Paul Clark <github@pdclark.com>
2014-01-21 19:54:17 -0700

    Update dropbox-404.php
    Replace Brainstorm Media with 10up.

--- dropbox-404.php
+++ dropbox-404.php
@@ -1,36 +1,14 @@
 <?php
 /*
 Plugin Name: Dropbox 404
-Plugin URI: http://brainstormmedia.com
+Plugin URI: http://github.com/10up/dropbox-404
 Description: Search Dropbox Public folder and various redirects for filenames before returning 404.
 Version: 1.0
-Author: Brainstorm Media
-Author URI: http://brainstormmedia.com
+Author: Paul Clark
+Author URI: http://pdclark.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
-
-/**
- * Copyright (c) 2012 Brainstorm Media. All rights reserved.
- *
- * Released under the GPL license
- * http://www.opensource.org/licenses/gpl-license.php
- *
- * This is an add-on for WordPress
- * http://wordpress.org/
- *
- * **********************************************************************
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * **********************************************************************
- */
-
-
 
 add_action('template_redirect', create_function('', 'global $storm_dropbox_404; $storm_dropbox_404 = new Storm_Dropbox_404();') );
 
